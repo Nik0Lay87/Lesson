0c05caeac50906df15aaf37833dd0e8a354a4497
Nikolay <Nik.kovaliov.1987@mail.ru>
2023-02-18 17:45:32 +0300

    Добавили папку Task18

---
+++ Seminar3/Task18/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number==1)
+{
+    Console.WriteLine("Координаты x > 0 v > 0 ");
+}
+else if (number==2)
+{
+    Console.WriteLine("Координаты x > 0 y < 0 ");
+}
+else if (number==3)
+{
+    Console.WriteLine("Координаты x < 0 y < 0 ");
+}
+else if (number==4)
+{
+    Console.WriteLine("Координаты x < 0 y > 0 ");
+}
+else  
+Console.WriteLine("Неправильный номер ");
