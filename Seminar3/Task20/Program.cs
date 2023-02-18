47adf304b853e4813877b54eb95babda085ad365
Nikolay <Nik.kovaliov.1987@mail.ru>
2023-02-18 18:07:17 +0300

    Добавили папку Task20

---
+++ Seminar3/Task20/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 20: Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,10
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координаты по оси x первой точки");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты по оси y первой точки");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты по оси x второй точки");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты по оси y второй точки");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double c = Math.Sqrt(Math.Pow (x2 - x1, 2) + Math.Pow (y2 - y1, 2));
+
+Console.WriteLine(c);
+
