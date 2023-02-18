bd60a4dc100e9fe6500408f467ae726cd3aa15a6
Nikolay <Nik.kovaliov.1987@mail.ru>
2023-02-18 12:35:53 +0300

    Добавили папку Seminar3

--- Seminar3/Task17/Program.cs
+++ Seminar3/Task17/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Введите координату точки по X");
 int x = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите координату точки по Y");
-int y = Convert.ToInt32(Console.ReadLine());
+int y = Conwert.ToInt32(Console.ReadLine());
 
 if(x > 0 && y > 0)
 {
