bbc0823a5d287adcb0be24adb3e0de17abe25510
Nikolay <Nik.kovaliov.1987@mail.ru>
2023-02-22 19:13:20 +0300

    Добавили папку Seminar4

---
+++ Seminar4/Task26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход 
+// число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Принять на вход число
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountOfDigits(int number)
+{
+    int count;
+    for(count = 0; number !=0; count++)
+    {
+       number /=10;  //можно записать так number = number / 10 
+    }
+    return count;
+}
+int userNumber = ReadNumber("Введите значение");
+int result = CountOfDigits(userNumber);
+Console.WriteLine(result);
