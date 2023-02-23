a48f3e382a2c62f1a407d71c39208c86b85055c2
Nikolay <Nik.kovaliov.1987@mail.ru>
2023-02-23 11:32:02 +0300

    Добавили папку Seminar4/Task28/

---
+++ Seminar4/Task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и
+// выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int SumToA(int a)
+{
+    int sum = 1;
+    for(int i = 1; i <=a; i++)
+    {
+        sum *= i;
+    }
+    return sum;
+}
+
+int number = ReadNumber("Введите число A");
+int result = SumToA(number);
+Console.WriteLine(result);
