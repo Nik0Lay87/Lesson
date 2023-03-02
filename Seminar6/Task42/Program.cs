99a1b6739e622049353820935fbbcdc94fc347b7
Nikolay <Nik.kovaliov.1987@mail.ru>
2023-03-02 11:58:32 +0300

    Добавили папку Seminar6

---
+++ Seminar6/Task42/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 42: Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+// Решение через циклы
+
+int n = 44;
+
+int countOfDigits = 0;
+int nCopy = n;
+
+while(nCopy != 0)
+{
+    nCopy /= 2;
+    countOfDigits++;
+}
+Console.WriteLine(countOfDigits);
+
+int[] binaryNumber = new int[countOfDigits];
+
+for(int i = 0; i < binaryNumber.Length; i++)
+{
+    binaryNumber[i] = n % 2;
+    n /= 2;
+}
+
+Console.WriteLine(string.Join("", binaryNumber));
+ReverseArray(binaryNumber);
+Console.WriteLine(string.Join("", binaryNumber));
+
+void ReverseArray(int[] array)
+{
+    for(int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+    }
+}
---
+++ Seminar6/Task44/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые
+// N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+const int N = 8;
+
+int[] Fibonacci = new int[N];
+
+Fibonacci[0] = 0;
+Fibonacci[1] = 1;
+
+for(int i = 2; i < Fibonacci.Length; i++)
+{
+    Fibonacci[i] = Fibonacci [i - 1] + Fibonacci[i - 2];
+}
+
+Console.WriteLine(string.Join(", ", Fibonacci));
